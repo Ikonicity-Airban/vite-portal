42f93dcd47c9b2400d5e9ac9aa93dc8cd2b6240a
Ikonicity Airban <ikonicityairban@gmail.com>
2023-10-05 07:39:58 +0100

    staff page

--- frontend/src/pages/staff.tsx
+++ frontend/src/pages/staff.tsx
@@ -25,9 +25,9 @@ function StaffPage() {
     <div className="mx-auto max-w-screen-desktop w-full my-10">
       <BreadcrumbComponents />
       <Section title="Staff Collection" subtitle="Our Amazing Staff">
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {data?.count ? (
-            data.instructors.map((ins, index) => (
+        {data?.count ? (
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {data.instructors.map((ins, index) => (
               <Card
                 key={index}
                 imgSrc={ins.photoURL || ""}
@@ -46,11 +46,11 @@ function StaffPage() {
                   {ins.courseTeaching?.code} {ins.courseTeaching?.title}
                 </p>
               </Card>
-            ))
-          ) : (
-            <Card>No Staff available</Card>
-          )}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <Card className="mx-auto">No Staff available</Card>
+        )}
       </Section>
     </div>
   );
